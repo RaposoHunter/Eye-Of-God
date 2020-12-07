f08d6154e1016fb7a7a9d720b1a4915f87e83fb6
Pedro Raposo <55608596+RaposoHunter@users.noreply.github.com>
2020-12-06 23:17:25 -0300

    Código atualizado para a versão mais atual

--- comandos/search.js
+++ comandos/search.js
@@ -1,5 +1,4 @@
 const https = require("https");
-const fs = require("fs");
 const gis = require("g-i-s");
 //const filter = require("../filter.js");
 
@@ -7,69 +6,55 @@ module.exports = {
 	name: "search",
 	description: "/search: Envia a primeira imagem encontrada sobre o que foi para o comando",
 	usage: "/search <pesquisa>",
-	execute: async (bot, chatId, args) => {
+	execute: async (bot, chatId, requester_username, args, isRand = false) => {
 		let search = args.join(" ");
-	
-		//let isPorn = filter(args[0]);
-		//if(isPorn) return bot.sendMessage(chatId, "PAREM DE VER PORNOGRAFIA");
-
+    
 		if(!search.length) return bot.sendMessage(chatId, "Você esqueceu de dizer o que queria pesquisar...");
 
-		const download = async (queue, path) => new Promise((resolve, reject) => {
-			gis(queue, (err, res) => {
-				if(err) console.log(err);
-				else {
+		const downloadAndSendToTelegram = async (queue, queueID) => {      
+      gis(queue, async (err, res) => {
+				if(err) { 
+					console.log(err);
+					bot.sendMessage(chatId, "Algo deu errado durante a execução do comando...");
+				} else {
 					let URL;
-					const file = fs.createWriteStream(path);
-					
-					for (let img of res) {
-						if (img.url[4] == "s") {
-							URL = img.url;
-							break;
-						}
-					}
-		
-					https.get(URL, res => {
-						res.pipe(file);
-		
-						file.on("finish", () => {
-							file.close(resolve(true));
-						});
-		
-						file.on("error", err => {
-							console.log(err);
-							fs.unlink(path, () => {});
-							reject(err.msg);
-						});
-					}).on("error", err => {
-						console.log(err);
-						fs.unlink(path, () => {});
-						reject(err.msg);
-					});
+					let notCompatible = true;
+          let img;
+          let i = 0;
+      
+          if(isRand) {
+            do {
+              let index = Math.floor(Math.random() * res.length);
+              img = res[index];
+
+              notCompatible = img.url.indexOf(".svg") != -1 || (img.url.indexOf(".png") == -1 && img.url.indexOf(".jpg") == -1);
+            } while(notCompatible);
+          } else {
+            do {
+              img = res[i];
+
+              notCompatible = img.url.indexOf(".svg") != -1 || (img.url.indexOf(".png") == -1 && img.url.indexOf(".jpg") == -1);
+              i++;
+            } while(notCompatible);
+          }
+          
+          URL = img.url;
+          
+          let newSearch = [];
+          args.forEach((str, i) => {
+            newSearch[i] = str.replace(str[0], str[0].toUpperCase()); 
+          });
+
+          newSearch = newSearch.join(" ");
+		      
+          await bot.sendMessage(chatId, `${requester_username}, aqui está uma imagem sobre "${newSearch}"": ${URL}`);
+          bot.deleteMessage(chatId, queueID);
 				}
 			});
-		});
+		};
 
-		const sendToTelegram = async (imgTitle, imgPath) => {
-			/*let args = imgTitle.split(" ");
-
-			for(let i in args) {
-				args[i][0].toUpperCase();
-			}
-
-			args = args.join(" ");*/
-			let args = imgTitle;
-
-			//await bot.sendMessage(chatId, args);
-			await bot.sendPhoto(chatId, imgPath);
-			await fs.unlink(imgPath, () => {});
-		}
-
-		bot.sendMessage(chatId, `Procurarei pela primeira imagem sobre "${search}". Aguarde... \u{231B}`);
-
-		const imgPath = `./imagens_temporarias/${search}.png`
-
-		await download(search, imgPath);
-		await sendToTelegram(search, imgPath);
+		let nMsg = await bot.sendMessage(chatId, `Procurarei pela primeira imagem sobre "${search}". Aguarde... \u{231B}`);
+  
+		await downloadAndSendToTelegram(search, nMsg.message_id);
 	}
 }
