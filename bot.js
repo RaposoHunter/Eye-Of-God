1ed136d19aa8d053d440614dd361a3b24b90897d
Pedro Raposo <55608596+RaposoHunter@users.noreply.github.com>
2020-12-06 23:19:54 -0300

    Código atualizado para a versão mais atual

--- bot.js
+++ bot.js
@@ -1,50 +1,28 @@
 require("dotenv").config();
-const fs = require("fs");
 
 const TelegramBot = require("node-telegram-bot-api");
 const token = process.env.TOKEN || null;
-const eyeOfGod = new TelegramBot(token, {polling: true});
+const eyeOfGod = new TelegramBot(token, { polling: true });
 
-eyeOfGod.commands = require("./commandsObj.js");
+eyeOfGod.search = require("./comandos/search.js");
 
-const commandFiles = fs.readdirSync("./comandos").filter(file => file.endsWith(".js"));
+eyeOfGod.on("message", msg => {  
+  const chatId = msg.chat.id;
+  const msgTxt = msg.text;
+  const requester_username = msg.from.username !== undefined ? "@" + msg.from.username : msg.from.first_name;
 
-for(let file of commandFiles) {
+  const args = msgTxt.trim().split(/ +/);
+  const cmd = args.shift();
 
-	let command = require(`./comandos/${file}`);
-	eyeOfGod.commands[command.name] = command;
-}
-
-eyeOfGod.onText(/\/(.+)/, (msg, match) => {
-	const chatId = msg.chat.id;
-	const hasAtSign = match[1].indexOf("@") != -1;
-	let args;
-	let cmd;
-
-	if(hasAtSign) {
-		args = match[1].trim().split("@GodsEye_chat_bot");
-		cmd = args.shift().toLowerCase();
-		args[0] = args[0].trim();
-		args = args[0].split(/ +/);
-		
-	} else {
-		args = match[1].trim().split(/ +/);
-		cmd = args.shift().toLowerCase();
-	}
-
-	if(!eyeOfGod.commands.has(cmd)) return eyeOfGod.sendMessage(chatId, "Desculpe, não conheço esse comando...");
-	
-	try {
-		eyeOfGod.commands[cmd].execute(eyeOfGod, chatId, args);
-	} catch(e) {
-		console.log(e);
-		eyeOfGod.sendMessage(chatId, "Algo deu errado na execução do comando...");
-	}
+  if (cmd == "/search" || cmd == "/search@GodsEye_chat_bot" || cmd == "/search_rand" || cmd == "/search_rand@GodsEye_chat_bot") {
+    let isRand = false;
+    if(cmd == "/search_rand" || cmd == "/search_rand@GodsEye_chat_bot") isRand = true;
+    
+    try {
+      eyeOfGod.search.execute(eyeOfGod, chatId, requester_username, args, isRand);
+    } catch (e) {
+      console.log(e);
+      eyeOfGod.sendMessage(chatId, "Algo deu errado na execução do comando...");
+    }
+  }
 });
-
-/*
-eyeOfGod.on("message", msg => {
-	const chatId = msg.chat.id;
-	console.log(msg);
-});
-*/
